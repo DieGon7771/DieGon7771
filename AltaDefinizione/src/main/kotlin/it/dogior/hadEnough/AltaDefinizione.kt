4d532115c98fc6906d29a2133fcf479172bf5d4c
DieGon7771 <bruttodiego6@gmail.com>
2026-01-08 14:23:00 +0100

    Update AltaDefinizione.kt

--- AltaDefinizione/src/main/kotlin/it/dogior/hadEnough/AltaDefinizione.kt
+++ AltaDefinizione/src/main/kotlin/it/dogior/hadEnough/AltaDefinizione.kt
@@ -24,7 +24,7 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class AltaDefinizione : MainAPI() {
-    override var mainUrl = "https://altadefinizionegratis.skin"
+    override var mainUrl = "https://altadefinizione.xn--6frz82g"
     override var name = "AltaDefinizione"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries, TvType.Documentary)
     override var lang = "it"
